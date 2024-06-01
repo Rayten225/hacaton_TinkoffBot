a3f898d14a78f77b287216565120411fa5c6b358
fra1ser <97356555+fra1ser@users.noreply.github.com>
2024-06-02 04:59:08 +0500

    Add files via upload

---
+++ gpt.py
@@ -0,0 +1,26 @@
+import requests
+idcm = "bt1vpbph6bfjm2f4d2ue" #id-дообученной модели
+IAM =  "AQVN3uisVvr7XhoXVLyvk90b8HFUp9wkGcB9i0WO" #значение IAM-токена сервисного аккаунта.
+folderId = "b1g72uajlds114mlufqi"
+def generate_message(query):
+    req = {
+            "modelUri": f"ds://{idcm}", #подключение к дообученной модели yandexgpt на датасете
+            "completionOptions": {
+                "stream": False,
+                "temperature": 0.1,
+                "maxTokens": "2000"
+            },
+            "messages": [
+                {
+                "role": "user",
+                "text": query
+                }
+            ]
+    }
+    headers = {"Authorization" : "Api-Key " + IAM,
+            "x-folder-id": folderId, }
+    response = requests.post("https://llm.api.cloud.yandex.net/foundationModels/v1/completion",
+        headers=headers, json=req)
+    if response.status_code == 200:
+        return response.text
+    return "bad request"
